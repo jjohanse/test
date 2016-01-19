d37624dcd67ac06b7aca0c0d20ee8379f5be49b9
jjohanse <jjohanse@lion.lmu.edu>
2016-01-19 12:37:18 -0800

    Delete Boomerang.java

--- Boomerang.java
+++
@@ -1,8 +0,0 @@
-class Boomerang {
-	public static void main (String args[]) {
-	double speed = 2.5;
-	double time = 10;
-	double distance = (time * speed) / 2;
-	System.out.println("Distance: " + distance + " u.m."); // u.m. = "units of measurement"
-	}
-}
