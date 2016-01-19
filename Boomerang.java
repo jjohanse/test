8fbd975d6792791fbd3c138fbde819a5c900f8a4
jjohanse <jjohanse@lion.lmu.edu>
2016-01-19 12:28:02 -0800

    change 1

--- Boomerang.java
+++ Boomerang.java
@@ -1,7 +1,7 @@
 class Boomerang {
 	public static void main (String args[]) {
-	double speed = 2.5;
-	double time = 10;
+	double speed = 40;
+	double time = 22;
 	double distance = (time * speed) / 2;
 	System.out.println("Distance: " + distance + " u.m."); // u.m. = "units of measurement"
 	}
